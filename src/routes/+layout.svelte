98bf600931833680423dcbcaac09a58d701ee763
Maxime <lasserre.maxime@yahoo.fr>
2022-11-22 12:07:25 +0100

    loading transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <style lang="scss" global>
   @use "src/lib/scss/all";
+
+  .loadingScreen {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+  }
 </style>
+
+<script>
+  import { navigating } from '$app/stores';
+  import { fade } from 'svelte/transition';
+</script>
+
+{#if $navigating}
+  <div class="loadingScreen" transition:fade>
+    <p>Chargement...</p>
+  </div>
+{/if}
 
 <slot />
