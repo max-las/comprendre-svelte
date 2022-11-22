599d05570409539e90428b265cc1bd7b7e41c6fa
Maxime <lasserre.maxime@yahoo.fr>
2022-11-22 17:15:43 +0100

    reorder

---
+++ src/routes/(pages)/+layout.svelte
@@ -0,0 +1,28 @@
+<style lang="scss">
+  nav {
+    border-bottom: 1px solid #ddd;
+    margin-bottom: 1em;
+  }
+
+  .navbar-brand {
+    img {
+      height: 24px;
+    }
+
+    & > * {
+      display: inline-block;
+      vertical-align: middle;
+    }
+  }
+</style>
+
+<nav class="navbar navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+      <img src="/icons/svelte.svg" alt="svelte icon">
+      <span>Comprendre Svelte</span>
+    </a>
+  </div>
+</nav>
+
+<slot />
