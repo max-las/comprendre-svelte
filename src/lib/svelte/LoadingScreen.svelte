d0bf3bcc2a87f562d7d36d33d2aee41b75327f3d
Maxime <lasserre.maxime@yahoo.fr>
2022-11-22 12:40:15 +0100

    animated transition

---
+++ src/lib/svelte/LoadingScreen.svelte
@@ -0,0 +1,22 @@
+<style lang="scss">
+  div {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: white;
+  }
+</style>
+
+<script>
+  import { fade } from 'svelte/transition';
+  import LoadingIcon from '$lib/svelte/LoadingIcon.svelte';
+</script>
+
+<div transition:fade>
+  <LoadingIcon />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,14 @@
 <style lang="scss" global>
   @use "src/lib/scss/all";
-
-  .loadingScreen {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: white;
-  }
 </style>
 
 <script>
   import { navigating } from '$app/stores';
-  import { fade } from 'svelte/transition';
+  import LoadingScreen from '$lib/svelte/LoadingScreen.svelte';
 </script>
 
 {#if $navigating}
-  <div class="loadingScreen" transition:fade>
-    <p>Chargement...</p>
-  </div>
+  <LoadingScreen />
 {/if}
 
 <slot />
