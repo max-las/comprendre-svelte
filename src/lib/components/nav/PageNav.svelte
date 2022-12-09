0ebbbbc839a74fdad528cf410eeb66fc2a3835eb
Maxime <lasserre.maxime@yahoo.fr>
2022-12-09 13:53:42 +0100

    component bottom nav, begin components explanation

---
+++ src/lib/components/nav/PageNav.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  type linkData = {
+    url: string,
+    text: string
+  };
+
+  export let before: linkData, after: linkData;
+</script>
+
+<div class="block">
+  {#if before}
+    <a class="btn btn-secondary" href="{before.url}">
+      <i class="bi bi-chevron-left"></i> {before.text}
+    </a>
+  {/if}
+
+  {#if after}
+    <a class="btn btn-primary" href="{after.url}">
+      {after.text} <i class="bi bi-chevron-right"></i>
+    </a>
+  {/if}
+</div>
