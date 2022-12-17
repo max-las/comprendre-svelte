21bad7a357f065cee53e99cf090f573ee31212a0
Maxime <lasserre.maxime@yahoo.fr>
2022-12-17 14:31:10 +0100

    begin list

---
+++ src/lib/demo/list/List.svelte
@@ -0,0 +1,50 @@
+<style>
+  .content {
+    max-width: 400px;
+  }
+
+  .task {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  form {
+    display: flex;
+    align-items: center;
+  }
+
+  button {
+    margin-left: 8px;
+  }
+</style>
+
+<script>
+  let taskList = [];
+  let lastId = 0;
+  let text = '';
+
+  const addTask = () => {
+    taskList = [...taskList, { text, id: ++lastId }];
+    text = '';
+    console.log(taskList);
+  };
+
+  const removeTask = (id) => {
+    taskList = taskList.filter((task) => task.id !== id);
+  }
+</script>
+
+<div class="content">
+  {#each taskList as task}
+    <div class="task shadow-sm">
+      {task.text}
+      <i class="bi bi-trash" on:click={() => removeTask(task.id)}></i>
+    </div>
+  {/each}
+
+  <form on:submit|preventDefault={addTask}>
+    <input class="form-control" type="text" bind:value={text}>
+    <button type="submit" class="btn btn-primary">Ajouter</button>
+  </form>
+</div>
+
